23f4a062cf6d3c601b92125a582b6da82422db93
JpAlamag <jpalamag@hotmail.com>
2016-03-01 18:12:35 -0500

    typescript on index fixed

--- Scripts/scenes/slotmachine.ts
+++ Scripts/scenes/slotmachine.ts
@@ -8,7 +8,7 @@ module scenes {
         private _bet100Button: objects.Button;
         private _spinButton: objects.Button;
         private _startOverButton: objects.Button;
-        private _quitButton: objects.Button
+        private _quitButton: objects.Button;
         private _reels: createjs.Bitmap[];
         private _jackpotText: objects.Label;
         private _creditsText: objects.Label;
